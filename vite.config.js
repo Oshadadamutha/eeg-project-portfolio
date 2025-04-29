72bbe70e6dbcba44fc1652ee48dbda86ca727a5e
Oshadadamutha <86254412+Oshadadamutha@users.noreply.github.com>
2025-04-29 23:43:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages';
 
-// https://vite.dev/config/
+// Replace with your GitHub repo name
+const repoName = 'your-repo-name';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ghPages()],
+  base: `/${repoName}/`,  // important for GitHub Pages
+});
