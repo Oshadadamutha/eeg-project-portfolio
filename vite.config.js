3932c1f323cba49d55db686740699bb6bbeac152
Oshadadamutha <86254412+Oshadadamutha@users.noreply.github.com>
2025-05-01 21:59:26 +0530

    deploy04

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/eeg-project-portfolio/",  // important for GitHub Pages
+  base: "/eeg-project-portfolio",  // important for GitHub Pages
 });
