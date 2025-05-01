4c650285ed22952f15e2d63c9799a78bba01ea57
Oshadadamutha <86254412+Oshadadamutha@users.noreply.github.com>
2025-05-02 01:50:05 +0530

    deploy08

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import '../styls/Navbar.css';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
 import { useState } from 'react';
 
 const Navbar = () => {
@@ -8,29 +9,29 @@ const Navbar = () => {
     <nav className="navbar">
       <h1>My Portfolio</h1>
       <ul className="nav-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
 
         <li 
           className="dropdown" 
           onMouseEnter={() => setDropdownOpen(true)} 
           onMouseLeave={() => setDropdownOpen(false)}
         >
-          <a href="/project-page">
+          <Link to="/project-page">
             Project Page ▾
-          </a>
+          </Link>
           {isDropdownOpen && (
             <ul className="dropdown-menu">
-              <li><a href="/week1">Week 1</a></li>
-              <li><a href="/week2">Week 2</a></li>
-              <li><a href="/week3">Week 3</a></li>
-              <li><a href="/week4">Week 4</a></li>
-              <li><a href="/week5">Week 5 and week 6</a></li>
-              <li><a href="/week7">Week 7 and week 8</a></li>
-              <li><a href="/week9">Week 9 and week 10</a></li>
-              <li><a href="/week11">Week 11</a></li>
-              <li><a href="/week12">Week 12</a></li>
-              <li><a href="/week13">Week 13</a></li>
+              <li><Link to="/week1">Week 1</Link></li>
+              <li><Link to="/week2">Week 2</Link></li>
+              <li><Link to="/week3">Week 3</Link></li>
+              <li><Link to="/week4">Week 4</Link></li>
+              <li><Link to="/week5">Week 5 and week 6</Link></li>
+              <li><Link to="/week7">Week 7 and week 8</Link></li>
+              <li><Link to="/week9">Week 9 and week 10</Link></li>
+              <li><Link to="/week11">Week 11</Link></li>
+              <li><Link to="/week12">Week 12</Link></li>
+              <li><Link to="/week13">Week 13</Link></li>
             </ul>
           )}
         </li>
