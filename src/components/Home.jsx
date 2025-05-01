34d08a005518334bc35cdc9c24ecf5831fc4ea4a
Oshadadamutha <86254412+Oshadadamutha@users.noreply.github.com>
2025-05-02 02:11:35 +0530

    deploy08

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,13 @@ import "../styls/Home.css"
 const Home = () => (
     <section id="home" className="text-center mt-10">
       <h2>Hi, I'm A.O.D. Silva</h2>
-      <p>Frontend Developer</p>
+      <p>BSc. Engineering (Electrical and Electronic) (UG) </p>
+      <p>University of Peradeniya</p>
       <img
-        src="/profile.jpg"
-        alt="Profile"
+        src="Profile.jpg"
         className="mx-auto w-40 h-40 rounded-full border-4 border-gray-300"
       />
-      <h2>Project Name</h2>
+      <h2>MI-based BCI</h2>
     </section>
   );
   export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import '../styles/Navbar.css';
+import '../styls/Navbar.css';
 import { useState } from 'react';
 
 const Navbar = () => {
