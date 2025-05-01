a6678e9d72f015b47a054f03eea3e7205a12b82d
Oshadadamutha <86254412+Oshadadamutha@users.noreply.github.com>
2025-05-01 21:38:48 +0530

    deploy01

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import ghPages from 'vite-plugin-gh-pages';
 
-// Replace with your GitHub repo name
-const repoName = 'eeg-project-portfolio';
 
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: `/${repoName}/`,  // important for GitHub Pages
+  base: "/eeg-project-portfolio",  // important for GitHub Pages
 });
